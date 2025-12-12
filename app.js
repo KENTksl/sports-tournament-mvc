c1776f1e6cd4b06e2c58d4dfd8e3c1e4132653ba
Minh <conkhi3695@gmail.com>
2025-12-12 14:26:27 +0700

    khởi tạo dự án web

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+//Body parser
+global.__basedir = __dirname;
+
+var bodyParser = require('body-parser')
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+//Controller
+var controller = require(__dirname  + "/apps/controllers");
+app.use(controller);
+
+app.set("views",__dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
+
+//Run server
+var server = app.listen(3000, function(){
+   console.log("server is running");
+});
